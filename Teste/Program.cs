9742fcbfb37d73a8090363adec610bddcf69761a
Brendon Genssinger Almeida Mascarenhas <brendon_genssinger@hotmail.com>
2017-02-20 23:31:30 -0300

    Ajustamos o arquivo config.

--- Teste/Program.cs
+++ Teste/Program.cs
@@ -5,15 +5,18 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 using System.Xml;
-
+using Teste.Model;
 namespace Teste
 {
     class Program
     {
         static void Main(string[] args)
         {
+            ParamentrosXml paramentosXml = new ParamentrosXml();
             RepositorioParamentrosXml.repositorio();
-            string[] arquivos = Directory.GetFiles("C:\\Desenvolvimento", "*.0001", SearchOption.AllDirectories);
+            string CaminhoDasImagens = '@'+'"' +paramentosXml.CaminhoDoArquivo.ToString()+ '"';
+            string CaminhoBackup = '@' + '"' + paramentosXml.CaminhoPastaTemp + '"';
+            string[] arquivos = Directory.GetFiles(CaminhoDasImagens, "*.0001", SearchOption.AllDirectories);
             Console.WriteLine(" LENDO ARQUIVOS ");
             int count = 0;
             string nome;
@@ -23,24 +26,34 @@ namespace Teste
             foreach (var item in arquivos)
             {                           
                 StringBuilder NovoNomeArquivo = new StringBuilder();
-                 nome = item.Substring(19,19).ToString();
+                nome = item.Substring(19,19).ToString();
                 LocalizaPalavraAcei = nome.Substring(0,4); // Localiza o Numero de protocolo
-                numeroProtocolo = nome.ToString().Substring(4,6); // Ler o numero de protocolo
+                numeroProtocolo = item.Substring(13,paramentosXml.QuantidadeDigitosProtocolo).ToString(); // Ler o numero de protocolo
+
                 if(LocalizaPalavraAcei.Equals("ACEI"))
                 {
                     return;
                 }
                 else
-                {                  
-
-                    NovoNomeArquivo.Append("C:\\Desenvolvimento\\");                    
+                {      
+                    NovoNomeArquivo.Append(@"D:\Teste\");                    
                     NovoNomeArquivo.Append("ACEI0000");
                     NovoNomeArquivo.Append(numeroProtocolo);
                     NovoNomeArquivo.Append(".0001");
-                    Console.WriteLine("Nome do Arquivo Antigo : {0}", item);                   
-
+                    Console.WriteLine("Nome do Arquivo Antigo : {0}", item);                  
                     try
                     {
+                        // Verifica se o arquivo de backup está criado, se não
+                        // Ele cria
+                        if(File.Exists(paramentosXml.CaminhoPastaTemp))
+                        {
+                            File.Copy(CaminhoDasImagens, CaminhoBackup);
+                        }
+                        else
+                        {
+                            File.Create(CaminhoBackup);
+                            File.Copy(CaminhoDasImagens, CaminhoBackup);
+                        }
                         
                         File.Move(item.ToString(), NovoNomeArquivo.ToString());
                         Console.WriteLine("Nome do Arquivo Novo : {0}", NovoNomeArquivo.ToString());
